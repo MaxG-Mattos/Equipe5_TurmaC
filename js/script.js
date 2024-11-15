6675b7232eb460a8ee6829d3d91e20b6f88c6c11
evilynav <evilynav@al.insper.edu.br>
2024-11-15 00:48:08 -0300

    familiarizacao

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const curtida = "img/afirmativo.png";
     const descurtida = "img/like.png"; 
 
-    likes.forEach(function(like, index) {
-        const savedState = localStorage.getItem(`likeState_${index}`); //esse likeState_(indique q a gnt quer) serve pra armazenar cada objeto separadamente, é tipo como  se fosse img[0], img[1] etc;
+    likes.forEach(function(like, i) {
+        const atual = localStorage.getItem(`likeState_${i}`); //esse likeState_(indique q a gnt quer) serve pra armazenar cada objeto separadamente, é tipo como  se fosse img[0], img[1] etc;
         
-        if (savedState === "curtido") {
+        if (atual === "curtido") {
             like.src = curtida;
         } else {
             like.src = descurtida;
@@ -16,10 +16,10 @@ document.addEventListener("DOMContentLoaded", () => {
         like.addEventListener("click", function() {
             if (like.src.includes(descurtida)) {
                 like.src = curtida;
-                localStorage.setItem(`likeState_${index}`, "curtido"); //localStorage: guardar as infos
+                localStorage.setItem(`likeState_${i}`, "curtido"); //localStorage: guardar as infos
             } else {
                 like.src = descurtida;
-                localStorage.setItem(`likeState_${index}`, "descurtido");
+                localStorage.setItem(`likeState_${i}`, "descurtido");
             }
         });
     });
